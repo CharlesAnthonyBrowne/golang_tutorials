2c7feb6db9b77f3d7b8740433ff7cc8196bd5be1
CharlesAnthonyBrowne <charles@charlesbrowne.io>
2016-06-18 08:46:43 +0100

    variables programme and binary programme

---
+++ variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var stringOne = "This is the first string without being initilized"
+
+	// := is to delcare a new variable. remove the : if the variable has already been set
+
+	stringTwo := "test"
+
+	fmt.Println(stringOne)
+	fmt.Println(stringTwo)
+}
